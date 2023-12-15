17cdd558fb44bc40627678e2699a7075672b4846
Håkon Hægland <hakon.hagland@gmail.com>
2023-12-15 22:51:24 +0100

    Select cell style from current styles
    Select color of cell from the available cell styles instead of
    hardcoding the style name.

--- scripts/src/fodt/add_keyword_status.py
+++ scripts/src/fodt/add_keyword_status.py
@@ -31,9 +31,31 @@ class AppendixStatusColorHandler(xml.sax.handler.ContentHandler):
         self.content = io.StringIO()
         self.current_keyword = None
         self.keyword_found = False
+        self.office_body_found = False
+        self.in_table_cell_style = False
+        self.orange_styles = set()
+        self.green_styles = set()
 
     def characters(self, content: str):
         self.content.write(xml.sax.saxutils.escape(content))
+
+    def collect_table_cell_styles(self, attrs: xml.sax.xmlreader.AttributesImpl) -> None:
+        # collect the style names for orange and green colors
+        # assume that attrs belongs to tagname "style:table-cell-properties" within
+        # tagname "style:style"
+        if "fo:background-color" in attrs.getNames():
+            color = attrs.getValue("fo:background-color")
+            if color == "#ff950e":
+                if self.current_style is not None:
+                    self.orange_styles.add(self.current_style)
+                else:
+                    logging.info(f"Warning: Found orange color without style name.")
+            elif color == "#579d1c":
+                if self.current_style is not None:
+                    self.green_styles.add(self.current_style)
+                else:
+                    logging.info(f"Warning: Found green color without style name.")
+
 
     def endElement(self, name: str):
         if not self.keyword_found:
@@ -41,50 +63,86 @@ class AppendixStatusColorHandler(xml.sax.handler.ContentHandler):
                 self.in_table_row = False
             elif self.in_table_row and name == "table:table-cell":
                 self.in_table_cell = False
+            elif self.in_table_cell_style and name == "style:style":
+                self.in_table_cell_style = False
         self.content.write(XMLHelper.endtag(name))
 
     def get_content(self) -> str:
         return self.content.getvalue()
+
+    def handle_table_row(
+            self, name: str, attrs: xml.sax.xmlreader.AttributesImpl
+    ) -> xml.sax.xmlreader.AttributesImpl:
+        # look for the table:table-row element, then look for table:table-cell within that
+        # element, then look for text:p within that element. Extract name of keyword from
+        # xlink:href attribute. Then look for the next table:table-cell element and replace
+        # the table:style-name attribute with the correct value according to the status value.
+        if name == 'table:table-row':
+            self.in_table_row = True
+            self.current_keyword = None
+        elif self.in_table_row and name == 'table:table-cell':
+            if (self.current_keyword is not None) and self.current_keyword == self.keyword:
+                logging.info(f"Found keyword {self.keyword}.")
+                # We have already found the keyword name within this table row
+                if "table:style-name" in attrs.getNames():
+                    attrs = {k: v for k, v in attrs.items()}
+                    if self.status == KeywordStatus.ORANGE:
+                        attrs["table:style-name"] = self.orange_style
+                    elif self.status == KeywordStatus.GREEN:
+                        attrs["table:style-name"] = self.green_style
+                    else:
+                        raise ValueError(f"Invalid status value: {self.status}.")
+                    logging.info(f"Successfully changed status of keyword {self.keyword}.")
+                    self.current_keyword = None
+                    self.in_table_cell = False
+                    self.keyword_found = True
+            else:
+                self.in_table_cell = True
+        elif self.in_table_cell and name == 'text:a':
+            if "xlink:href" in attrs.getNames():
+                href = attrs.getValue("xlink:href")
+                # the href value is on the form "#1.2.1.ACTDIMS – ACTION Keyword Dimensions"
+                # we want to extract the keyword name from this string
+                if match := re.match(r"#\d+.\d+.\d+.(\w+)\s+", href):
+                    self.current_keyword = match.group(1)
+        return attrs
+
+    def select_table_cell_styles(self) -> None:
+        if len(self.orange_styles) > 0:
+            logging.info(f"Found {len(self.orange_styles)} orange styles.")
+            self.orange_style = self.orange_styles.pop()
+            logging.info(f"Using orange style {self.orange_style}.")
+        else:
+            logging.info(f"Warning: No orange styles found.")
+            self.orange_style = None
+        if len(self.green_styles) > 0:
+            logging.info(f"Found {len(self.green_styles)} green styles.")
+            self.green_style = self.green_styles.pop()
+            logging.info(f"Using green style {self.green_style}.")
+        else:
+            logging.info(f"Warning: No green styles found.")
+            self.green_style = None
+
 
     def startDocument(self):
         self.content.write(XMLHelper.header)
 
     def startElement(self, name:str, attrs: xml.sax.xmlreader.AttributesImpl):
         if not self.keyword_found:
-            # look for the table:table-row element, then look for table:table-cell within that
-            # element, then look for text:p within that element. Extract name of keyword from
-            # xlink:href attribute. Then look for the next table:table-cell element and replace
-            # the table:style-name attribute with the correct value according to the status value.
-            if name == 'table:table-row':
-                self.in_table_row = True
-                self.current_keyword = None
-            elif self.in_table_row and name == 'table:table-cell':
-                if (self.current_keyword is not None) and self.current_keyword == self.keyword:
-                    logging.info(f"Found keyword {self.keyword}.")
-                    # We have already found the keyword name within this table row
-                    if "table:style-name" in attrs.getNames():
-                        attrs = {k: v for k, v in attrs.items()}
-                        if self.status == KeywordStatus.ORANGE:
-                            # Orange table cell has style name "Table35.H9"
-                            attrs["table:style-name"] = "Table35.H9"
-                        elif self.status == KeywordStatus.GREEN:
-                            # Green table cell has style name "Table35.H5"
-                            attrs["table:style-name"] = "Table35.H5"
-                        else:
-                            raise ValueError(f"Invalid status value: {self.status}.")
-                        logging.info(f"Successfully changed status of keyword {self.keyword}.")
-                        self.current_keyword = None
-                        self.in_table_cell = False
-                        self.keyword_found = True
-                else:
-                    self.in_table_cell = True
-            elif self.in_table_cell and name == 'text:a':
-                if "xlink:href" in attrs.getNames():
-                    href = attrs.getValue("xlink:href")
-                    # the href value is on the form "#1.2.1.ACTDIMS – ACTION Keyword Dimensions"
-                    # we want to extract the keyword name from this string
-                    if match := re.match(r"#\d+.\d+.\d+.(\w+)\s+", href):
-                        self.current_keyword = match.group(1)
+            if not self.office_body_found:
+                if name == 'style:style':
+                    if "style:family" in attrs.getNames():
+                        if attrs.getValue("style:family") == "table-cell":
+                            self.in_table_cell_style = True
+                            self.current_style = attrs.getValue("style:name")
+                elif self.in_table_cell_style and name == 'style:table-cell-properties':
+                    self.collect_table_cell_styles(attrs)
+                elif name == 'office:body':
+                    logging.info(f"Found office:body.")
+                    self.select_table_cell_styles()
+                    self.office_body_found = True
+            else:
+                attrs = self.handle_table_row(name, attrs)
         self.content.write(XMLHelper.starttag(name, attrs))
 
 
